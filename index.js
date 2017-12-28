f5ef81e0f114c784be9e230a5ba330a52bfb6f30
Alexander <MrRandomgamerdkHD@gmail.com>
2017-12-28 16:03:01 +0100

    Implemented a basic blockchain

--- index.js
+++ index.js
@@ -1,12 +1,76 @@
 const Blocks = require( "blockchain-lite" )
+const Crypto = require('crypto');
 
 let Block = Blocks.basic
 
-let b0 = Block.first( 'Genesis' )
-let b1 = Block.next( b0, 'Some json' )
-let b2 = Block.next( b1, 'Some json......' )
-let b3 = Block.next( b2, 'Even more json...' )
+class AbelothBlockChain {
+  //let chain = []
+  //let currentBlock = {}
+  //let genBlock = {}
+  constructor (){
+    this.chain = []
+    this.currentBlock = {}
+    this.genBlock = Block.first('She is ancient, and powerful, and dangerous. Very dangerous.')
+    this.currentBlock = this.genBlock
+    this.chain.push(this.genBlock)
+  }
 
-let blockchain = [b0, b1, b2, b3]
+   createBlock(data) {
+    let block = Block.next(this.currentBlock, data)
+    this.addBlockToChain(block)
+  }
 
-console.log( blockchain )
+  addBlockToChain(block) {
+    if (this.validateBlock(block, this.currentBlock)) {
+      this.chain.push(block)
+      this.currentBlock = block
+      return true
+    }
+    return false
+  }
+
+  validateBlock(newBlock, latestBlock) {
+    if (latestBlock.index + 1 != newBlock.index) {
+      return false
+    } else if (latestBlock.hash != newBlock.previousHash ) {
+      return false
+    } else if (!this.validateHash(newBlock)) {
+      return false
+    }
+
+    //TODO: Add more validation for data
+
+    return true
+  }
+
+  validateHash(block) {
+    return (this.createHash(block) == block.hash)
+  }
+
+  getLatestBlock() {
+    return this.currentBlock
+  }
+
+  createHash(block) {
+    return Crypto.createHash('SHA256').update(block.index.toString()+block.timestamp.toString()+block.data.toString()+block.previousHash).digest('hex');
+  }
+
+  getChainLength() {
+    return this.chain.length
+  }
+
+  getChain() {
+    return this.chain
+  }
+}
+
+let ab = new AbelothBlockChain()
+ab.createBlock('1')
+ab.createBlock('12')
+ab.createBlock('13')
+ab.createBlock('14')
+
+console.log("Chain:")
+console.log(ab.getChain());
+
+console.log("Chain length: " + ab.getChainLength());
